dcf4ea3ae99b742fb45f038c8a8c018576edf027
Berfu İpek <ber_fu_95@hotmail.com>
2017-04-08 11:58:54 -0700

    hesap makinesi eklendi

---
+++ hesapmakinesi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace hesapmakinesidouble
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sayı2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayı1, sayı2, toplam;
+            sayı1 = Convert.ToDouble(textBox1.Text);
+            sayı2 = Convert.ToDouble(textBox2.Text);
+
+            toplam = sayı1 + sayı2;
+            label4.Text = toplam.ToString();
+
+
+        }
+    }
+}
